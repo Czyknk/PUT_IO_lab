3e783eeb32e819b49083846c31a091c6a82d57c3
Czyknk <jedrek.mikolajczyk@gmail.com>
2023-10-24 09:17:59 +0200

    new object

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,8 +1,12 @@
+//
 public class Main {
   int x = 5;
 
   public static void main(String[] args) {
-    Main myObj = new Main();
-    System.out.println(myObj.x);
+    Main myObj1 = new Main();  // Object 1
+    Main myObj2 = new Main();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
   }
 }
+
