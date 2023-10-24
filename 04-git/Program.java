a65a2ebca499c494ea4306ba401eada10b4c68d0
Czyknk <jedrek.mikolajczyk@gmail.com>
2023-10-24 09:19:19 +0200

    delete object

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -4,9 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     Main myObj1 = new Main();  // Object 1
-    Main myObj2 = new Main();  // Object 2
     System.out.println(myObj1.x);
-    System.out.println(myObj2.x);
   }
 }
 
